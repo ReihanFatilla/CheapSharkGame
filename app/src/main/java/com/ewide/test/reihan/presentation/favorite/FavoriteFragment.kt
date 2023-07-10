c4b2ae1f93846e9f232af3b80119e4ac7db3f3e7
ReihanFatilla <reihanfatilla@gmail.com>
2023-07-10 21:26:13 +0700

    [feat]: changed Concept to Bottom navigation Bar

---
+++ app/src/main/java/com/ewide/test/reihan/presentation/favorite/FavoriteFragment.kt
@@ -0,0 +1,18 @@
+package com.ewide.test.reihan.presentation.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ewide.test.reihan.R
+
+class FavoriteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+}
